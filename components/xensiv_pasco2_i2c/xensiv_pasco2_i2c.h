96b373ea09513c3309b083afa32a7afde64c0e4c
Michal Gora <michal.gora@infineon.com>
2025-11-10 16:51:50 +0100

    dumbdown for testing

--- components/xensiv_pasco2_i2c/xensiv_pasco2_i2c.h
+++ components/xensiv_pasco2_i2c/xensiv_pasco2_i2c.h
@@ -4,22 +4,18 @@
 #include "esphome/components/sensor/sensor.h"
 #include "esphome/components/i2c/i2c.h"
 
-namespace esphome
-{
-    namespace xensiv_pasco2_i2c
-    {
+namespace esphome {
+namespace xensiv_pasco2_i2c {
 
-        class XensivPasCO2I2C : public sensor::Sensor, public PollingComponent, public i2c::I2CDevice
-        {
-        public:
-            void setup() override;
-            void update() override;
-            void dump_config() override;
-            void read_co2_ppm();
-            
-            protected:
-            float co2_ppm_{0};
-            uint16_t version_{0};
-        };
-    }
-}
+class XensivPasCO2I2C : public PollingComponent, public i2c::I2CDevice, public sensor::Sensor {
+ public:
+  void setup() override;
+  void update() override;
+  void dump_config() override;
+
+ protected:
+  float co2_ppm_{0.0f};
+};
+
+}  // namespace xensiv_pasco2_i2c
+}  // namespace esphome
