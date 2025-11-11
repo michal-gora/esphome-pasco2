a5e19abc8e1b99d804afa60039e3d954bd32880b
Michal Gora <michal.gora@infineon.com>
2025-11-11 16:16:31 +0100

    fix

--- components/xensiv_pasco2_i2c/xensiv_pasco2_i2c.cpp
+++ components/xensiv_pasco2_i2c/xensiv_pasco2_i2c.cpp
@@ -60,6 +60,22 @@ namespace esphome
             {
                 ESP_LOGW(TAG, "Failed to read MEAS_STS register (0x07)");
             }
+            // Clear MEAS_STS
+            uint8_t int_sts_clr_mask = 0x0;
+            arg->write_byte(0x07, int_sts_clr_mask);
+            // Read again to verify clearing
+            meas_sts_val = 0;
+            if (this->read_bytes(0x07, &meas_sts_val, 1))
+            {
+                ESP_LOGD(TAG, "MEAS_STS (0x07) after clear: 0x%02X", meas_sts_val);
+            }
+            else
+            {
+                ESP_LOGW(TAG, "Failed to read MEAS_STS register (0x07)");
+            }
+
+
+
             uint8_t int_cfg_val = 0;
             if (this->read_bytes(0x08, &int_cfg_val, 1))
             {
