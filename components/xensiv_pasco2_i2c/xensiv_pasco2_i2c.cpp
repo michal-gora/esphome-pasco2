e9516afc274d86d7e7849eec425c4fb2733748af
Michal Gora <michal.gora@infineon.com>
2025-11-11 15:22:09 +0100

    update now sets op mode and int

--- components/xensiv_pasco2_i2c/xensiv_pasco2_i2c.cpp
+++ components/xensiv_pasco2_i2c/xensiv_pasco2_i2c.cpp
@@ -44,7 +44,7 @@ namespace esphome
             //     ESP_LOGD(TAG, "Data ready from interrupt");
             //     this->data_ready_ = false;
             // }
-            
+            set_continuous_operation_mode_with_interrupt_()
             this->read_co2_ppm();
 
         }
